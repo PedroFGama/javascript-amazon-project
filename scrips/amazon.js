3046c71d034c4a66d3ba08d6fefd8961d88a5a23
PedroFGama <pedro.fariasgama@gmail.com>
2025-10-03 20:35:20 -0500

    Ao atualizar a quantidade no carrinho, agora é adicionado as unidades a partir quantity-selector

--- scrips/amazon.js
+++ scrips/amazon.js
@@ -26,7 +26,7 @@ products.forEach((product) => {
           </div>
 
           <div class="product-quantity-container">
-            <select>
+            <select class="js-quantity-selector-${product.id}">
               <option selected value="1">1</option>
               <option value="2">2</option>
               <option value="3">3</option>
@@ -58,12 +58,14 @@ products.forEach((product) => {
 
 document.querySelector('.js-products-grid').innerHTML = productsHTML;
 
+
+
 document.querySelectorAll('.js-add-to-cart')
   .forEach((button) => {
     button.addEventListener('click', () => {
 
       const productId = button.dataset.productId;
-      
+      const quantitySelected = document.querySelector(`.js-quantity-selector-${productId}`);
       let matchingItem;
 
       cart.forEach((item) => {
@@ -75,11 +77,11 @@ document.querySelectorAll('.js-add-to-cart')
       });
 
       if (matchingItem) {
-        matchingItem.quantity += 1;
+        matchingItem.quantity += Number(quantitySelected.value);
       } else {
         cart.push({
           productId: productId,
-          quantity: 1
+          quantity: Number(quantitySelected.value)
         });
       }
       
